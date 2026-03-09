36ce76e6a271f659543ddb5a5095439f4292c047
Sayanth <sayanthstrng@gmail.com>
2026-03-09 15:15:32 +0530

    struct

---
+++ new-folder/intro.go
@@ -0,0 +1,48 @@
+/*
+We use structs in Go to represent structured data. It's often convenient to group different types of variables together. For example, if we want to represent a car we could do the following:
+
+type car struct {
+Make string 
+Model string 
+Height int 
+Width int 
+
+}
+
+This creates a new struct type called car. All cars have a Make, Model, Height and Width.
+
+In Go, you will often use a struct to represent information that you would have used a dictionary for in Python, or an object literal for in JavaScript.
+
+*/
+
+package main 
+import "fmt"
+
+
+type messageToSend struct {
+  phoneNumber int
+  message string
+
+}
+
+
+func test(m messageToSend) {
+	fmt.Printf("Sending message: '%s' to: %v\n", m.message, m.phoneNumber)
+	fmt.Println("==========================")
+}
+
+
+func main() {
+	test(messageToSend{
+		phoneNumber: 148255510981,
+		message:     "Thanks for signing up",
+	})
+	test(messageToSend{
+		phoneNumber: 148255510982,
+		message:     "Love to have you aboard!",
+	})
+	test(messageToSend{
+		phoneNumber: 148255510983,
+		message:     "We're so excited to have you",
+	})
+}
