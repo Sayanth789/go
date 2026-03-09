30b58ba34de3d67c887e694ff28cd52da362db33
Sayanth <sayanthstrng@gmail.com>
2026-03-09 13:47:05 +0530

    type inference

---
+++ new-folder/variables/typeInference.go
@@ -0,0 +1,20 @@
+/*
+To declare a variable without specifying an explicit type (either by using the := syntax or var = expression syntax), 
+the variable's type is inferred from the value on the right hand side.
+
+When the right hadnd side of the declaration is typed, the new variable is of that same type.
+
+
+var i int
+j := i // j is also an int
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	penniesPerText := 0.02
+	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+}
