d7f4ef2cc3cb181cea5a5f0f93baf2809453263a
Sayanth <sayanthstrng@gmail.com>
2026-03-10 17:11:10 +0530

    buffrered channels

---
+++ new-folder/channel/buffred_channels.go
@@ -0,0 +1,43 @@
+/* 
+You can provide a buffer length as the second argument to make() to create a buffered channel:
+
+ch := make(chan int, 100)
+
+Sending on a buffered channel only blocks when the buffer is full.
+
+Receiving blocks only when the buffer is empty.
+
+*/
+package main
+
+import "fmt"
+
+func addEmailsToQueue(emails []string) chan string {
+	emailsToSend := make(chan string, len(emails))
+	for _, email := range emails {
+		emailsToSend <- email
+	}
+	return emailsToSend
+}
+
+
+func sendEmails(batchSize int, ch chan string) {
+	for i := 0; i < batchSize; i++ {
+		email := <-ch
+		fmt.Println("Sending email:", email)
+	}
+}
+
+func test(emails ...string) {
+	fmt.Printf("Adding %v emails to queue...\n", len(emails))
+	ch := addEmailsToQueue(emails)
+	fmt.Println("Sending emails...")
+	sendEmails(len(emails), ch)
+	fmt.Println("==========================================")
+}
+
+func main() {
+	test("Hello John, tell Kathy I said hi", "Whazzup bruther")
+	test("I find that hard to believe.", "When? I don't know if I can", "What time are you thinking?")
+	test("She says hi!", "Yeah its tomorrow. So we're good.", "Cool see you then!", "Bye!")
+}
